890cb8924ccedbdcef0cb04bc24e54ff952d71e6
ch3rr17 <cherr.batac@gmail.com>
2018-01-07 14:35:32 -0800

    initial commit with files prior to authentication component

---
+++ src/app/app.module.ts
@@ -0,0 +1,86 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ClientService } from './services/client.service';
+
+// Create Routes
+const appRoutes: Routes =
+[
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'add-client',
+    component: AddClientComponent
+  },
+  {
+    path: 'client/:id',
+    component: ClientDetailsComponent
+  },
+  {
+    path: 'edit-client/:id',
+    component: EditClientComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    RegisterComponent,
+    LoginComponent,
+    SettingsComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FlashMessagesModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase, 'ClientPanel'),
+    AngularFireAuthModule
+  ],
+  providers: [
+    AngularFireDatabase,
+    AngularFireDatabaseModule,
+    ClientService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD_ozBaY6zC4fDEW167fdbSil4uQ6R6V48',
+    authDomain: 'clientapp-10b98.firebaseapp.com',
+    databaseURL: 'https://clientapp-10b98.firebaseio.com',
+    projectId: 'clientapp-10b98',
+    storageBucket: 'clientapp-10b98.appspot.com',
+    messagingSenderId: '408404289619'
+  }
+};
